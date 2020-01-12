7417ce7e9ee9c966ba94ef50254cf74fc543d531
Ovidiu Miru <ovimiru@gmail.com>
2020-01-12 10:31:43 +0200

    First test.

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World new\n');
+    res.end('Hello World\n');
 });
+
 server.listen(PORT, () => {
+    // eslint-disable-next-line
     console.log(`Server running on ${PORT}/`);
 });
+
+// server.listen(PORT, () => {
+//     console.log(`Server running on ${PORT}/`);
+// });
