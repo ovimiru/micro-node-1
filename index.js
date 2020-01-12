ab876d986e5edbd9556ceb2b77ed6a2c37e341b7
Ovidiu Miru <ovimiru@gmail.com>
2020-01-12 11:19:54 +0200

    Test Heroku starter app

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const http = require('http');
-const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-// server.listen(PORT, () => {
-//     // eslint-disable-next-line
-//     console.log(`Server running on ${PORT}/`);
-// });
-
-server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}/`);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
