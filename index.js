c5fd3ace59d65ba318297a43b624cf9659e32daf
Ovidiu Miru <ovimiru@gmail.com>
2020-01-12 10:03:59 +0200

    no message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.end('Hello World new\n');
 });
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT}/`);
