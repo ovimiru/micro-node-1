e1b39694833465d53c54fab888742497f761ae28
Ovidiu Miru <ovimiru@gmail.com>
2020-01-16 21:42:32 +0200

    no message

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const courses = [
+    { id: 1, name: "Algorithms" },
+    { id: 2, name: "Software Engineering" },
+    { id: 3, name: "Human Computer Interaction" }
+];
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get("/courses", function(req, res) {
+    res.send(courses);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
