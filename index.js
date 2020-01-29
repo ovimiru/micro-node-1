7aee87c138555b0f55d610466b2cc4e1aa70474c
Ovidiu <ovimiru@gmail.com>
2020-01-29 15:20:50 +0200

    no message

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const axios = require('axios');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 let courses = [];
-
-// console.log('here');
-
-// Make a request for a user with a given ID
-axios.get('https://micro-node-unitbv-2.herokuapp.com/courses')
-    .then(function (response) {
-        // handle success
-        // console.log(response.data);
-        courses = response.data;
-    })
-    .catch(function (error) {
-        // handle error
-        // console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    });
-
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/courses', function(req, res) {
-    res.send(JSON.stringify(courses));
+  .get('/students', function(req, res) {
+    axios.get('https://micro-node-unitbv-2.herokuapp.com/users')
+          .then(function (response) {
+              // courses = response.data;
+              results = response.data;
+              // res.send(JSON.stringify(courses));
+              res.render('pages/db', results );
+          });
+      // res.send(JSON.stringify(courses));
       // res.render('pages/db', results );
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
